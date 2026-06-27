93ee8fd67ad709cde342aea49eb9d352f1005920
Numan Razi <razinuman333@gmail.com>
2026-06-27 15:34:33 +0400

    clerk integrated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 import assets from '../assets/assets'
 import {MenuIcon, SearchIcon, XIcon} from 'lucide-react'
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 
 
 function Navbar() {
 
   const [isOpen,setIsOpen]=useState(false)
-
+  const {user}=useUser()
+  const {openSignIn}=useClerk()
 
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
@@ -31,9 +33,15 @@ function Navbar() {
       <div className='flex items-center gap-8'>
 
      <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
-      <button className='px-4 py-1 sm:px-7 sm:py-2 bg-primary
+    {
+      !user ? (
+          <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary
       hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
 
+      ) : (
+        <UserButton/>
+      )
+    }
       </div>
 
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,20 @@
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
+import { ClerkProvider } from "@clerk/clerk-react";
 import './index.css'
 import App from './App.jsx'
 
+
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!clerkPubKey) {
+   throw new Error('Missing Publisheble Key')
+}
+
 createRoot(document.getElementById('root')).render(
- <BrowserRouter>
-    <App />
- </BrowserRouter>
+   <ClerkProvider publishableKey={clerkPubKey}>
+      <BrowserRouter>
+         <App />
+      </BrowserRouter>
+   </ClerkProvider>
+
 )
