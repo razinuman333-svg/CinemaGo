0afb9a693075fed99c85a0ea102a8a5712d7c157
Numan Razi <razinuman333@gmail.com>
2026-06-28 09:50:22 +0400

    blur circle created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import Feautered from '../components/Feautered'
 
 function Home() {
   return (
     <>
      <Hero/>
-   
+     <Feautered/>
     </>
   )
 }
