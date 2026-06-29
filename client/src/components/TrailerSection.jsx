ec3a21940fbbe9aec1de0ae1138f2917f32dc752
Numan Razi <razinuman333@gmail.com>
2026-06-29 07:44:53 +0400

    the trailer section completed

--- client/src/components/TrailerSection.jsx
+++ client/src/components/TrailerSection.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { dummyTrailers } from '../assets/assets'
 import ReactPlayer from 'react-player/youtube'
 import Blur from './Blur'
+import {PlayCircleIcon} from 'lucide-react'
 
 function TrailerSection() {
 
@@ -18,11 +19,14 @@ function TrailerSection() {
           </div>
 
 
-          <div className=' bg-red-900 group grid grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto'>
+          <div className='  group grid grid-cols-4 gap-4 md:gap-8 mt-8 max-w-3xl mx-auto'>
             {dummyTrailers.map((trailer)=>(
               <div key={trailer.image} className='relative group-hover:not-hover:opacity-50 hover:-translate-y-1 duration-300
               transition max-md:h-60 md:max-h-60 cursor-pointer' onClick={()=>setCurrentTrailer(trailer)}>
-                <img src={trailer.image} className=''/>
+
+                <img src={trailer.image} className='rounded-lg w-full h-full object-cover brightness-75'/>
+                <PlayCircleIcon className='absolute top-1/2 left-1/2 w-5 md:w-8 h-5 md:h-12 transform -translate-x-1/2
+                -translate-y-1/2'/>
 
               </div>
               
