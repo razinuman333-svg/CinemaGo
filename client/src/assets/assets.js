b5d4a703846096cea821805e2f37c7d1ee55c496
Numan Razi <razinuman333@gmail.com>
2026-07-01 18:12:52 +0400

    the movie details page done

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -26,6 +26,26 @@ export const dummyCastsData = [
     name: "Chris Hemsworth",
     profile_path: "https://image.tmdb.org/t/p/original/jpurJ9jAcLCYjgHHfYF32m3zJYm.jpg",
   },
+   {
+    name: "Tom Holland",
+    profile_path: "https://image.tmdb.org/t/p/original/2qhIDp44cAqP2clOgt2afQI07X8.jpg",
+  },
+   {
+    name: "Tom Holland",
+    profile_path: "https://image.tmdb.org/t/p/original/2qhIDp44cAqP2clOgt2afQI07X8.jpg",
+  },
+   {
+    name: "Tom Holland",
+    profile_path: "https://image.tmdb.org/t/p/original/2qhIDp44cAqP2clOgt2afQI07X8.jpg",
+  },
+   {
+    name: "Tom Holland",
+    profile_path: "https://image.tmdb.org/t/p/original/2qhIDp44cAqP2clOgt2afQI07X8.jpg",
+  },
+   {
+    name: "Tom Holland",
+    profile_path: "https://image.tmdb.org/t/p/original/2qhIDp44cAqP2clOgt2afQI07X8.jpg",
+  },
 ];
 
 export const dummyTrailers = [
@@ -173,5 +193,38 @@ export const dummyShowsData = [
     runtime: 176,
   },
 ];
+
+
+export const dummyDateTimeData = {
+  "2025-07-24": [
+    { time: "2025-07-24T01:00:00.000Z", showId: "68395b407f6329be2bb45bd1" },
+    { time: "2025-07-24T03:00:00.000Z", showId: "68395b407f6329be2bb45bd2" },
+    { time: "2025-07-24T05:00:00.000Z", showId: "68395b407f6329be2bb45bd3" },
+  ],
+
+  "2025-07-25": [
+    { time: "2025-07-25T01:00:00.000Z", showId: "68395b407f6329be2bb45bd4" },
+    { time: "2025-07-25T03:00:00.000Z", showId: "68395b407f6329be2bb45bd5" },
+    { time: "2025-07-25T05:00:00.000Z", showId: "68395b407f6329be2bb45bd6" },
+  ],
+
+  "2025-07-26": [
+    { time: "2025-07-26T01:00:00.000Z", showId: "68395b407f6329be2bb45bd7" },
+    { time: "2025-07-26T03:00:00.000Z", showId: "68395b407f6329be2bb45bd8" },
+    { time: "2025-07-26T05:00:00.000Z", showId: "68395b407f6329be2bb45bd9" },
+  ],
+
+  "2025-07-27": [
+    { time: "2025-07-27T01:00:00.000Z", showId: "68395b407f6329be2bb45bda" },
+    { time: "2025-07-27T03:00:00.000Z", showId: "68395b407f6329be2bb45bdb" },
+    { time: "2025-07-27T05:00:00.000Z", showId: "68395b407f6329be2bb45bdc" },
+  ],
+
+  "2025-07-28": [
+    { time: "2025-07-28T01:00:00.000Z", showId: "68395b407f6329be2bb45bdd" },
+    { time: "2025-07-28T03:00:00.000Z", showId: "68395b407f6329be2bb45bde" },
+    { time: "2025-07-28T05:00:00.000Z", showId: "68395b407f6329be2bb45bdf" },
+  ],
+};
 
 export default assets;
