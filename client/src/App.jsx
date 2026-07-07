131f969cd1806ea9ca1adef6085f2d81d83cb2b1
Numan Razi <razinuman333@gmail.com>
2026-07-07 11:02:04 +0400

    isoTimeFormat Lib Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
      <Route path='/' element={<Home/>}/>
      <Route path='/movies' element={<Movies/>}/>
      <Route path='/movies/:id' element={<MovieDetails/>}/>
-     <Route path='/movies/:id/:id' element={<SeatLayout/>}/>
+     <Route path='/movies/:id/:date' element={<SeatLayout/>}/>
      <Route path='/my-bookings' element={<Mybookings/>}/>
      <Route path='/favorites' element={<Favorite/>}/>
 
--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
+import Loading from '../components/Loading'
+import { ClockIcon } from 'lucide-react'
+import isoTimeFormat from '../lib/isoTimeFormat'
 
 function SeatLayout() {
-  return (
-    <div>
-      
+
+  const { id, date } = useParams()
+  const [show, setShow] = useState()
+  const [selectedTime,setSelectedTime]=useState()
+
+
+
+
+
+  const findShow = async () => {
+    const show = dummyShowsData.find(mov => mov._id == id)
+    if (show) {
+      setShow({
+        movie: show,
+        dateTime: dummyDateTimeData
+      })
+    }
+  }
+
+  useEffect(() => {
+    findShow()
+  }, [])
+
+
+
+  return show ? (
+    <div className='flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50'>
+      {/* available timings  */}
+   <div className='w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 h-max md:sticky md:top-30'>
+    <p className='text-lg font-semibold px-6'>Available Timings</p>
+    <div className='mt-5 space-y-1'>
+      {show.dateTime[date].map((item,index)=>(
+        <div key={index} className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition
+        ${selectedTime?.time == item.time ? 'bg-primary text-white' : 'hover:bg-primary/20'}`}>
+       <ClockIcon className='w-4 h-4'/>
+       <p className='text-sm'>{isoTimeFormat(item.time)}</p>
+        </div>
+      ))}
+
     </div>
+   </div>
+
+   {/* seatLayout */}
+   <div></div>
+
+    </div>
+  ) : (
+    <Loading />
   )
 }
 
