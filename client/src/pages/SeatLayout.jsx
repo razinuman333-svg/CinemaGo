fb305b1be5fd565ec4b648dd97da893d51aa087d
Numan Razi <razinuman333@gmail.com>
2026-07-07 20:11:49 +0400

    seat layout page done

--- client/src/pages/SeatLayout.jsx
+++ client/src/pages/SeatLayout.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import assets, { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import Loading from '../components/Loading'
-import { ClockIcon } from 'lucide-react'
+import { ArrowRight, ClockIcon } from 'lucide-react'
 import isoTimeFormat from '../lib/isoTimeFormat'
 import Blur from '../components/Blur'
 import toast from 'react-hot-toast'
 
 function SeatLayout() {
 
+
+  const navigate=useNavigate()
   const { id, date } = useParams()
   const [show, setShow] = useState()
   const [selectedTime,setSelectedTime]=useState()
@@ -70,17 +72,22 @@ function SeatLayout() {
 
   return show ? (
     
-    <div className='flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50'>
+    <div className=' flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50'>
 
+ 
+ 
+ 
+ 
+ 
       {/* available timings  */}
-   <div className='w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 h-max md:sticky md:top-30'>
+   <div className=' w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 h-max md:sticky md:top-30'>
 
     <p className='text-lg font-semibold px-6'>Available Timings</p>
 
     <div className='mt-5 space-y-1'>
 
       {show.dateTime[date].map((item,index)=>(
-        <div onClick={()=>setSelectedTime(item)} key={index} className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition
+        <div onClick={()=>setSelectedTime(item)} key={index} className={` flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition
         ${selectedTime?.time == item.time ? 'bg-primary text-white' : 'hover:bg-primary/20'}`}>
        <ClockIcon className='w-4 h-4'/>
        <p className='text-sm'>{isoTimeFormat(item.time)}</p>
@@ -91,19 +98,40 @@ function SeatLayout() {
     </div>
    </div>
 
+
+
+
+
    {/* seatLayout */}
-   <div className='relative flex-1 flex flex-col items-center max-md:mt-16'>
+   <div className=' relative  flex-1 flex flex-col items-center max-md:mt-16'>
     <Blur top='-100px' left='-100px'/>
     <Blur bottom='0' right='0'/>
     <h1 className='text-2xl font-semibold mb-4'>Select Your Seat</h1>
     <img className='w-90 h-20' src={assets.screen}/>
     <p>Screen Side</p>
-    <div className='flex flex-col items-center mt-10 text-xs text-gray-300'>
-      <div className='grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6'>
+    <div className=' flex flex-col items-center mt-10 text-xs text-gray-300'>
+      <div className=' grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6'>
       {groupRows[0].map((item)=>renderSeats(item))}
       </div>
+   <div className='grid grid-cols-2 gap-11'>
+      {groupRows.slice(1).map((item,index)=>(
+        <div key={index} className=''>
+         {item.map((row)=>renderSeats(row))}
+        </div>
+
+      ))}
 
     </div>
+    </div>
+
+
+
+    <button onClick={()=>navigate('/my-bookings')} className='flex items-center gap-1 mt-20 px-10 py-3 text-sm bg-primary hover:bg-primary-dull
+    transition rounded-full font-medium cursor-pointer active:scale-95'>
+      Proceed To Checkout
+      <ArrowRight strokeWidth={3} className='w-4 h-4'/>
+      </button>
+   
    </div>
 
     </div>
