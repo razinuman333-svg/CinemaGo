95175a6b345b76dabff41ddd0b8c8636ad149abd
Numan Razi <razinuman333@gmail.com>
2026-07-08 16:35:09 +0400

    dateFormat lib Created

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -228,5 +228,46 @@ export const dummyDateTimeData = {
     { time: "2025-07-28T05:00:00.000Z", showId: "68395b407f6329be2bb45bdf" },
   ],
 };
+export const dummyBookingData = [
+  {
+    "_id": "68396334fb83252d82e17295",
+    "user": { "name": "GreatStack" },
+    "show": {
+      "_id": "68352363e96d99513e4221a4",
+      "movie": dummyShowsData[0],
+      "showDateTime": "2025-06-30T02:30:00.000Z",
+      "showPrice": 59
+    },
+    "amount": 98,
+    "bookedSeats": ["D1", "D2"],
+    "isPaid": false
+  },
+  {
+    "_id": "68396334fb83252d82e17295", 
+    "user": { "name": "GreatStack" },
+    "show": {
+      "_id": "68352363e96d99513e4221a4",
+      "movie": dummyShowsData[0],
+      "showDateTime": "2025-06-30T02:30:00.000Z",
+      "showPrice": 59
+    },
+    "amount": 49,
+    "bookedSeats": ["A1"],
+    "isPaid": true
+  },
+  {
+    "_id": "68396334fb83252d82e17295",
+    "user": { "name": "GreatStack" },
+    "show": {
+      "_id": "68352363e96d99513e4221a4",
+      "movie": dummyShowsData[0],
+      "showDateTime": "2025-06-30T02:30:00.000Z",
+      "showPrice": 59
+    },
+    "amount": 147,
+    "bookedSeats": ["A1", "A2", "A3"],
+    "isPaid": true
+  }
+];
 
 export default assets;
--- client/src/pages/Mybookings.jsx
+++ client/src/pages/Mybookings.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyBookingData } from '../assets/assets'
+import Loading from '../components/Loading'
+import Blur from '../components/Blur'
 
 function Mybookings() {
-  return (
-    <div>
+
+  const currency=import.meta.env.VITE_CURRENCY
+  const [bookings,setBookings]=useState()
+  const [isLoading,setIsLoading]=useState(true)
+
+
+  const getMyBookings=async()=>{
+     setBookings(dummyBookingData)
+     setIsLoading(false)
+  }
+
+  useEffect(()=>{
+      getMyBookings()
+  },[])
+
+  return !isLoading ? (
+    <div className='relative px-6 md:px-16 lg:px-40 pt-30 md:pt-40 min-h-[80vh]'>
+      <Blur top='100px' left='100px'/>
+      <div>
+        <Blur bottom='0px' left='600px'/>
+      </div>
+      <h1 className='text-lg font-semibold mb-4'>My Bookings</h1>
+      {bookings.map((item,index)=>(
+        <div key={index} className='flex flex-col md:flex-row justify-between bg-primary/8 border border-primary/20
+        rounded-lg mt-4 p-2 max-w-3xl'>
+          <div className='flex flex-col md:flex-row'>
+            <img className='md:max-w-45 aspect-video h-auto object-cover object-bottom rounded' src={item.show.movie.poster_path}/>
+            <div className='flex flex-col p-4'>
+            <p>{item.show.movie.title}</p>
+            </div>
+
+          </div>
+
+        </div>
+      ))}
       
     </div>
+  ) : (
+    <Loading/>
   )
 }
 
