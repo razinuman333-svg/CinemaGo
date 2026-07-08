8493a74c8ad58244594a928ca5b82d28291d1a3f
Numan Razi <razinuman333@gmail.com>
2026-07-08 19:58:43 +0400

    the admin navbar done

--- client/src/components/Admin/AdminNavbar.jsx
+++ client/src/components/Admin/AdminNavbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import assets from '../../assets/assets'
 
 function AdminNavbar() {
   return (
-    <div>
+    <div className='flex items-center justify-between px-6 md:px-10 h-16 border-b border-gray-300/30'>
+      <Link to='/'>
+      <img className='w-36 h-auto' src={assets.logo}/>
+      </Link>
       
     </div>
   )
