f07991a0561ea2f39a7b279f7d5a284a5330e68f
Numan Razi <razinuman333@gmail.com>
2026-07-08 19:48:47 +0400

    Admin Layout Done

---
+++ client/src/components/Admin/AdminSideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminSideBar() {
+  return (
+    <div>
+      <h1>sidu</h1>
+    </div>
+  )
+}
+
+export default AdminSideBar
--- client/src/pages/Admin/Layout.jsx
+++ client/src/pages/Admin/Layout.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import AdminNavbar from '../../components/Admin/AdminNavbar'
+import AdminSideBar from '../../components/Admin/AdminSideBar'
+import { Outlet } from 'react-router-dom'
 
 function Layout() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <AdminNavbar/>
+      <div className='flex'>
+        <AdminSideBar/>
+        <div className='flex-1 px-4 py-10 md:px-10 h-[calc(100vh-64px] overflow-y-auto'>
+            <Outlet/>
+        </div>
+
+      </div>
+    </>
   )
 }
 
