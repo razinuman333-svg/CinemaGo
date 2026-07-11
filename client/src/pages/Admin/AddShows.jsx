3ba377912c4814fc458ba5aa4c651985ef779f18
Numan Razi <razinuman333@gmail.com>
2026-07-11 07:33:52 +0400

    the k converter function created in lib folder

--- client/src/pages/Admin/AddShows.jsx
+++ client/src/pages/Admin/AddShows.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { dummyShowsData } from '../../assets/assets'
+import Loading from '../../components/Loading'
+import Text from '../../components/Admin/Text'
 
 function AddShows() {
-  return (
-    <div>
+
+
+  const currency=import.meta.env.VITE_CURRENCY
+  const [nowPlayngMovies,setNowPlayingMovies]=useState()
+  const [selectedMovies,setSelectedMovies]=useState()
+  const [dateTimeSelection,setDateTimeSelection]=useState()
+  const [dateTimeInput,setDateTimeInput]=useState()
+  const [showPrice,setShowPrice]=useState()
+
+
+  const fetchNowPlayingMovies=()=>{
+    setNowPlayingMovies(dummyShowsData)
+  }
+
+
+
+
+  useEffect(()=>{
+    fetchNowPlayingMovies()
+  },[])
+
+
+
+  return nowPlayngMovies.length > 0 ? (
+    <>
+
+    <Text text1='Add' text2='Shows'/>
       
-    </div>
-  )
+    </>
+  ) : <Loading/>
 }
 
 export default AddShows
