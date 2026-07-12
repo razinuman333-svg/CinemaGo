9aa45d0a403493332a91e9097f62041d05d3dffe
Numan Razi <razinuman333@gmail.com>
2026-07-12 19:56:31 +0400

    the innget setup dine functions declared

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
+import { clerkMiddleware } from '@clerk/express'
 
 
 const app = express()
@@ -13,6 +14,7 @@ await connectDB()
 // Middleware
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 // API routes
 app.get('/',(req,res)=>res.send('Numaneyy You Will Be SuccessFull'))
