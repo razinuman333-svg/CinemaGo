ed8277c0b18f54712bf73d569e7ef67a3a536627
Numan Razi <razinuman333@gmail.com>
2026-07-12 09:58:49 +0400

    mongodb atlas connection done

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/db.js'
 
 
 const app = express()
 const port = 3000
+
+
+await connectDB()
 
 // Middleware
 app.use(express.json())
