564bfcb93643c27ba794212ff82cb4b081969ca4
Numan Razi <razinuman333@gmail.com>
2026-07-12 22:19:16 +0400

    Add inngest endpoint and functions

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,70 +1,82 @@
-import {Inngest} from 'inngest'
-import User from '../models/User.js'
+import { Inngest } from "inngest";
+import User from "../models/User.js";
 
+export const inngest = new Inngest({
+  id: "movie-ticket-booking",
+});
 
-export const inngest = new Inngest({id:"movie-ticket-booking"})
-
-
-
-// inngest function to save user data to database 
+// Create user
 const syncUserCreation = inngest.createFunction(
-    {id:"sync-user-from-clerk"},
-    {event:"clerk/user.created"},
-    async({event})=>{
-        const {id,first_name,last_name,email_addresses,image_url}=event.data
-        const userData={
-            _id:id,
-            email:email_addresses[0].email_address,
-            name:first_name + " " +last_name,
-            image:image_url
-        }
-        await User.create(userData)
+  {
+    id: "sync-user-from-clerk",
+    triggers: {
+      event: "clerk/user.created",
+    },
+  },
+  async ({ event }) => {
+    const {
+      id,
+      first_name,
+      last_name,
+      email_addresses,
+      image_url,
+    } = event.data;
 
-    }
-)
+    const userData = {
+      _id: id,
+      email: email_addresses[0].email_address,
+      name: `${first_name ?? ""} ${last_name ?? ""}`.trim(),
+      image: image_url,
+    };
 
+    await User.create(userData);
+  }
+);
 
-
-
-
-
-
-// Inngest function to delete user from database
+// Delete user
 const syncUserDeletion = inngest.createFunction(
-    {id:"delete-user-with-clerk"},
-    {event:"clerk/user.deleted"},
-    async({event})=>{
-       const {id}=event.data
-       await User.findByIdAndDelete(id)
+  {
+    id: "delete-user-with-clerk",
+    triggers: {
+      event: "clerk/user.deleted",
+    },
+  },
+  async ({ event }) => {
+    const { id } = event.data;
 
-    }
-)
+    await User.findByIdAndDelete(id);
+  }
+);
 
-
-
-
-
-
-// inngest function to Update user data to database 
+// Update user
 const syncUserUpdation = inngest.createFunction(
-    {id:"update-user-from-clerk"},
-    {event:"clerk/user.updated"},
-    async({event})=>{
-        const {id,first_name,last_name,email_addresses,image_url}=event.data
-        const userData={
-            _id:id,
-            email:email_addresses[0].email_address,
-            name:first_name + " " +last_name,
-            image:image_url
-        }
-        await User.findByIdAndUpdate(id,userData)
+  {
+    id: "update-user-from-clerk",
+    triggers: {
+      event: "clerk/user.updated",
+    },
+  },
+  async ({ event }) => {
+    const {
+      id,
+      first_name,
+      last_name,
+      email_addresses,
+      image_url,
+    } = event.data;
 
-    }
-)
+    const userData = {
+      email: email_addresses[0].email_address,
+      name: `${first_name ?? ""} ${last_name ?? ""}`.trim(),
+      image: image_url,
+    };
 
+    await User.findByIdAndUpdate(id, userData);
+  }
+);
 
-
-
-
-export const functions = [syncUserCreation,syncUserDeletion,syncUserUpdation]
-    
+export const functions = [
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+];
--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+
+import { inngest, functions } from "./inngest/index.js";
 
 
 const app = express()
@@ -15,6 +18,14 @@ await connectDB()
 app.use(express.json())
 app.use(cors())
 app.use(clerkMiddleware())
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions,
+  })
+);
+
 
 // API routes
 app.get('/',(req,res)=>res.send('Numaneyy You Will Be SuccessFull'))
