add926d0e185182bcad295e0e3550979ff2423f7
Numan Razi <razinuman333@gmail.com>
2026-07-15 21:27:52 +0400

    userRouter

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import { inngest, functions } from "./inngest/index.js";
 import showRouter from './routes/showRoutes.js'
 import bookingRouter from './routes/bookingRoutes.js'
 import adminRouter from './routes/adminRoutes.js'
+import userRouter from './routes/userRoutes.js'
 
 
 const app = express()
@@ -36,6 +37,7 @@ app.get('/',(req,res)=>res.send('Numaneyy You Will Be SuccessFull'))
 app.use('/api/show',showRouter)
 app.use('/api/booking',bookingRouter)
 app.use('/api/admin',adminRouter)
+app.use('/api/user',userRouter)
 
 
 
